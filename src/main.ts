5dbb31697a14f588db0cb69ccfcf0750318cc3fc
huqzc <yeshuqz@gmail.com>
2024-07-03 17:40:54 +0800

    feat(setting): add settings item

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,57 @@
 import { Plugin, WorkspaceLeaf } from 'obsidian'
-import { SampleSettingTab } from "./page/setting/setting-tab";
+import { SampleSettingTab } from './page/setting/setting-tab'
 import { GitCommitter, VIEW_TYPE_NAME } from './page/committer/commit-tab'
 
+interface PluginSettings {
+  remoteRepoUrl: string
+  operationMeetGieRepo: string
+}
+
+const DEFAULT_SETTINGS: PluginSettings = {
+  remoteRepoUrl: '',
+  operationMeetGieRepo: 'ignore'
+}
+
 export default class GitCommitterPlugin extends Plugin {
-    onload() {
-        this.addSettingTab(new SampleSettingTab(this.app, this));
-        this.registerView(VIEW_TYPE_NAME, leaf => new GitCommitter(leaf))
+  settings: PluginSettings
 
-        this.addRibbonIcon('git-commit-horizontal', 'git', () => {
-            this.activateView()
-        })
+  async onload() {
+    await this.loadSettings()
+    this.addSettingTab(new SampleSettingTab(this.app, this))
+    this.registerView(VIEW_TYPE_NAME, leaf => new GitCommitter(leaf))
+
+    this.addRibbonIcon('git-commit-horizontal', 'git', () => {
+      this.activateView()
+    })
+  }
+
+  onunload() {}
+
+  async activateView() {
+    const { workspace } = this.app
+
+    let leaf: WorkspaceLeaf | null = null
+    const leaves = workspace.getLeavesOfType(VIEW_TYPE_NAME)
+
+    if (leaves.length > 0) {
+      // A leaf with our view already exists, use that
+      leaf = leaves[0]
+    } else {
+      // Our view could not be found in the workspace, create a new leaf
+      // in the right sidebar for it
+      leaf = workspace.getRightLeaf(false)
+      await leaf?.setViewState({ type: VIEW_TYPE_NAME, active: true })
     }
-    onunload() {
-    }
 
-    async activateView() {
-        const { workspace } = this.app
+    // "Reveal" the leaf in case it is in a collapsed sidebar
+    workspace.revealLeaf(leaf as WorkspaceLeaf)
+  }
 
-        let leaf: WorkspaceLeaf | null = null
-        const leaves = workspace.getLeavesOfType(VIEW_TYPE_NAME)
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+  }
 
-        if (leaves.length > 0) {
-            // A leaf with our view already exists, use that
-            leaf = leaves[0]
-        } else {
-            // Our view could not be found in the workspace, create a new leaf
-            // in the right sidebar for it
-            leaf = workspace.getRightLeaf(false)
-            await leaf?.setViewState({ type: VIEW_TYPE_NAME, active: true })
-        }
-
-        // "Reveal" the leaf in case it is in a collapsed sidebar
-        workspace.revealLeaf(leaf as WorkspaceLeaf)
-    }
+  async saveSettings() {
+    await this.saveData(this.settings)
+  }
 }
--- src/page/setting/setting-tab.ts
+++ src/page/setting/setting-tab.ts
@@ -1,18 +1,42 @@
-import { App, PluginSettingTab } from "obsidian";
-import { createApp } from "vue";
-import SamplePlugin from "../../main";
-import SampleSettingTabPage from "./SettingTabPage.vue";
-
+import { App, PluginSettingTab, Setting } from 'obsidian'
+import SamplePlugin from '../../main'
 
 export class SampleSettingTab extends PluginSettingTab {
-    plugin: SamplePlugin;
-    constructor(app: App, plugin: SamplePlugin) {
-        super(app, plugin);
-        this.plugin = plugin;
-    }
-    display() {
-        const { containerEl } = this;
-        containerEl.empty();
-        createApp(SampleSettingTabPage).mount(containerEl);
-    }
+  plugin: SamplePlugin
+
+  constructor(app: App, plugin: SamplePlugin) {
+    super(app, plugin)
+    this.plugin = plugin
+  }
+
+  display() {
+    const { containerEl } = this
+    containerEl.empty()
+    // createApp(SampleSettingTabPage).mount(containerEl)
+
+    new Setting(containerEl)
+      .setName('Remote repository url')
+      .setDesc('The remote repository url of this vault.')
+      .addText(text => {
+        text
+          .setPlaceholder('https://github.com/your-username/your-repo.git')
+          .setValue(this.plugin.settings.remoteRepoUrl)
+          .onChange(async value => {
+            this.plugin.settings.remoteRepoUrl = value
+            await this.plugin.saveSettings()
+          })
+      })
+
+    new Setting(containerEl)
+      .setName('Submit other git repositories')
+      .setDesc('Submit the source code of other git repositories in this vault.')
+      .addDropdown(dropdown => {
+        dropdown.addOption('0', 'NO')
+        dropdown.addOption('1', 'YES')
+        dropdown.onChange(async value => {
+          this.plugin.settings.operationMeetGieRepo = value
+          await this.plugin.saveSettings()
+        })
+      })
+  }
 }
